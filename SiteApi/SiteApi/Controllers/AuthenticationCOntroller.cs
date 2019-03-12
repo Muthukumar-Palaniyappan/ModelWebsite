bc603c22e6ec08b601607fe06160a35afe3af4f9
muthumeenal-palaniappan <plmeenu@gmail.com>
2019-03-12 23:25:53 +1100

    Initial commit for the WebAPI using .net core

---
+++ SiteApi/SiteApi/Controllers/AuthenticationCOntroller.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SiteApi.Controllers
+{
+    public class AuthenticationController:Controller
+
+    {
+        public string Authenticate(string userName, string password)
+        {
+            string jwtToken=null;
+            return jwtToken;
+        }
+
+    }
+}
